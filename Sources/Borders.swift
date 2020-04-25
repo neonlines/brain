9879725b9f8ad57af9ee4d046905ec58fbc88352
vauxhall <zero.griffin@gmail.com>
2020-04-25 12:12:48 +0200

    Add radius parameter

--- Sources/Borders.swift
+++ Sources/Borders.swift
@@ -1,6 +1,7 @@
 import CoreGraphics
 
 public struct Borders {
+    public let radius: CGFloat
     let min: CGFloat
     let max: CGFloat
     let spacing = CGFloat(100)
@@ -8,5 +9,6 @@ public struct Borders {
     public init(radius: CGFloat) {
         max = radius - spacing
         min = -max
+        self.radius = radius
     }
 }
